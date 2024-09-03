4834674621a15e77ffd63b2d177a57fee3eb25dd
sanara <sanara@cpqd.com.br>
2024-09-03 16:46:49 -0300

    adicionado implementação com banco de dados jpa

--- Curso 1 - SpringBoot 3 - API Rest em Java/api_voll/src/main/java/med/voll/api/controller/MedicoController.java
+++ Curso 1 - SpringBoot 3 - API Rest em Java/api_voll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,20 +1,39 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
 
-    @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
-    }
+	@Autowired
+	private MedicoRepository repository;
 
+	@PostMapping
+	@Transactional
+	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+		repository.save(new Medico(dados));
+	}
+
+	@GetMapping
+	public List<DadosListagemMedico> listar() {
+		// itera sobre a lista de médicos convertendo em um dto com os dados
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+	}
 
 }
