bdf53603187dc94d5638781ed11dcd35a41948f3
sanara <sanara@cpqd.com.br>
2025-02-27 15:05:06 -0300

    incluindo gateway, balanceamento de carga serviço de descoberta

--- Curso 5 - Microservicos Spring Java/pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
+++ Curso 5 - Microservicos Spring Java/pagamentos/src/main/java/br/com/alurafood/pagamentos/PagamentosApplication.java
@@ -2,8 +2,11 @@ package br.com.alurafood.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //Essa anotação serve para ele saber que é um cliente e vai se registrar em algum Eureka.
+ // Eureka adicionado na pom na versao : org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.1.2
 public class PagamentosApplication {
 
 	public static void main(String[] args) {
