d0051e96147f2dcb1d0a9cf3c4d15ac40ad3c043
sanara <sanara@cpqd.com.br>
2025-03-06 15:14:02 -0300

    subindo conteiner da imagem no dockerhub e conectando ao banco de dados.

--- Curso 6 IaC com CDK e deploy na AWS/pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
+++ Curso 6 IaC com CDK e deploy na AWS/pedidos/src/main/java/br/com/alurafood/pedidos/PedidosApplication.java
@@ -2,10 +2,9 @@ package br.com.alurafood.pedidos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient removendo o Eureka da api
 public class PedidosApplication {
 
 	public static void main(String[] args) {
