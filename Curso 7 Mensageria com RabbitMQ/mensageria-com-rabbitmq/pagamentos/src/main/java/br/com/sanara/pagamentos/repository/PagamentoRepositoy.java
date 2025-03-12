9723d681b77b458248e01efbec6d8f465ef7ba63
sanara <sanara@cpqd.com.br>
2025-03-12 14:56:35 -0300

    publicando mensagens no RabbitMQ com Spring

--- Curso 7 Mensageria com RabbitMQ/mensageria-com-rabbitmq/pagamentos/src/main/java/br/com/sanara/pagamentos/repository/PagamentoRepositoy.java
+++
@@ -1,7 +0,0 @@
-package br.com.sanara.pagamentos.repository;
-
-import br.com.sanara.pagamentos.model.Pagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PagamentoRepositoy extends JpaRepository<Pagamento, Long> {
-}
