36ddf76af757e3d97f362a19018e1ffd200dabc0
sanara <sanara@cpqd.com.br>
2025-03-26 09:14:00 -0300

    criando um job, step e um reader

---
+++ Curso 9 Processamento em lote com Spring Batch/spring-batch/src/main/java/br/com/alura/codetickets/ImportacaoJobConfiguration.java
@@ -0,0 +1,75 @@
+package br.com.alura.codetickets;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class ImportacaoJobConfiguration {
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    /**
+     * cria o job responsável pela execução de leitura do arquivo csv
+     * @param passoInicial
+     * @param jobRepository
+     * @return
+     */
+    @Bean
+    public Job job(Step passoInicial, JobRepository jobRepository) {
+        return new JobBuilder("geracao-tickets", jobRepository)
+                .start(passoInicial)
+                .incrementer(new RunIdIncrementer()) //incrementa a cada execucao desse job
+                .build();
+    }
+
+    /**
+     * Cria o passo inicial
+     * <Importacao, Importacao> indica qual dado esta lendo e qual esta escrevendo nesse caso sao iguais
+     * chunk(200, transactionManager) indica qtos dados serao processados por bloco de transacao, faz o envio/roolback de 200 em 200
+     * @param reader
+     * @param writer
+     * @param jobRepository
+     * @return
+     */
+    @Bean
+    public Step passoInicial(ItemReader<Importacao> reader, ItemWriter<Importacao> writer, JobRepository jobRepository) {
+        return new StepBuilder("passo-inicial", jobRepository)
+                .<Importacao, Importacao>chunk(200, transactionManager)
+                .reader(reader) //faz a leitura
+                .writer(writer) //faz a escrita
+                .build();
+    }
+
+    /**
+     * Faz a leitura do arquivo csv dentro da pasta files conforme atributos da classe importacao
+     * A configuração do FlatFileItemReader (tipo para csv) deve especificar o caractere delimitador do arquivo CSV,
+     * via chamada ao metodo .delimiter(";"). Caso contrário, a leitura dos dados será incorreta.
+     * @return
+     */
+    @Bean
+    public ItemReader<Importacao> reader() {
+        return new FlatFileItemReaderBuilder<Importacao>()
+                .name("leitura-csv")
+                .resource(new FileSystemResource("files/dados.csv"))
+                .comments("--") //faz com q se quiser passar um comentario toda linha q inicia com -- é uma linha de comentario
+                .delimited()
+                .names("cpf", "cliente", "nascimento", "evento", "data", "tipoIngresso", "valor")
+                .targetType(Importacao.class)
+                .build();
+    }
+
+
+}
