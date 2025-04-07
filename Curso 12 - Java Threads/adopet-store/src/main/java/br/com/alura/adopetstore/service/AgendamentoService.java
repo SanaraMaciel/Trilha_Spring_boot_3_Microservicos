090e09ca785c676828ed395b65a926627ec532c3
sanara <sanara@cpqd.com.br>
2025-04-07 10:33:59 -0300

    paralelizando threads com CompletableFuture

--- Curso 12 - Java Threads/adopet-store/src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
+++ Curso 12 - Java Threads/adopet-store/src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 @Service
 public class AgendamentoService {
 
@@ -15,16 +18,26 @@ public class AgendamentoService {
     private EmailDeRelatorioGerado enviador;
 
     //anotacao para configurar o horario que ira executar a tarefa
-    @Scheduled(cron = "0 40 9 * * *") //* * * não tem dia, mes ou ano então vai todos dias
+    @Scheduled(cron = "0 07 10 * * *") //* * * não tem dia, mes ou ano então vai todos dias
     public void envioEmailsAgendado() {
         var estoqueZerado = relatorioService.infoEstoque();
         var faturamentoObtido = relatorioService.faturamentoObtido();
 
-        enviador.enviar(estoqueZerado, faturamentoObtido);
+        //o join(). Esse metodo permite que a thread principal (main) aguarde a conclusão de outras threads
+        // secundárias antes de prosseguir. deixando as threads sincronizadas
+        CompletableFuture.allOf(estoqueZerado, faturamentoObtido).join();
+
+        try {
+            enviador.enviar(estoqueZerado.get(), faturamentoObtido.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println("thread do agendamento: " + Thread.currentThread().getName());
 
         //System.out.println(estoqueZerado);
         //System.out.println(faturamentoObtido);
+
+
     }
 }
